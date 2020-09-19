7494911889ddfafde856330f33e6f401a5008e2f
swap-etc <71559668+swap-etc@users.noreply.github.com>
2020-09-19 21:53:48 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,39 @@
-const apiUrl = `https://web-api.coinmarketcap.com/v1/tools/price-conversion?amount=100&convert_id=2781&id=1`;
-fetch(apiUrl, {
-  headers: {
-    accept: "application/json, text/plain, */*",
-    "accept-language": "en-US,en;q=0.9",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-  },
-  referrer: "https://coinmarketcap.com/",
-  referrerPolicy: "origin",
-  body: null,
-  method: "GET",
-  mode: "no-cors",
-  credentials: "omit",
-}).then(console.log);
+function handleChange() {
+  const inputAmount = +document.getElementById("inputAmount").value;
+  if (inputAmount < 50) {
+    swal("Sorry!", "Minimum amount is 50 ETC!", "error");
+    document.getElementById("inputAmount").value = 50;
+    return;
+  }
+  const inputType = document.getElementById("inputType").value;
+  if (inputType === "USD") {
+    document.getElementById("totlaAmount").value = 4.5 * inputAmount;
+    return;
+  }
+  const apiUrl = `https://api.gdax.com/products/${inputType}-USD/book`
+  fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => {
+      const price = +data.bids[0][0];
+      document.getElementById("totlaAmount").value = (1 / price) * 4.5 * inputAmount;
+    });
+}
+
+function handleContinue() {
+  document.getElementById("step-1").hidden = true;
+  document.getElementById("step-2").hidden = false;
+}
+
+function handleSubmit() {
+  document.getElementById("step-1").hidden = false;
+  document.getElementById("step-2").hidden = true;
+  swal("Congrats!", "Your exchange is going on its way!", "success");
+}
+
+function handleCopy() {
+  var copyText = document.getElementById("qr-code")
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+}
+handleChange();
